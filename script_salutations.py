97eace01b77d0f232cdce1ac02a3c291ebcc8bb0
Marlene Sanjose <marlene.sanjose@etsmtl.net>
2024-05-15 10:50:00 -0400

    Mise à jour des messages utilisateurs plus polis

--- script_salutations.py
+++ script_salutations.py
@@ -2,10 +2,10 @@
 Ce script affiche des messages de bienvenue
 """
 
-last_name = input('Quel est ton nom de famille ?')
-first_name = input('Quel est ton prenom ?')
+last_name = input('Quel est votre nom de famille ?')
+first_name = input('Quel est votre prenom ?')
 
-message = f'Bienvenue à toi {first_name} {last_name} !'
+message = f'Bienvenue {first_name} {last_name} ! Nous sommes très fiers de vous comptez parmi nous'
 
 print(message)
 
